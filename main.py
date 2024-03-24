08f9de09f63ac29c4647dcbd106f262eb16688f0
KieVitz <s217442@sggw.edu.pl>
2024-03-24 01:36:44 +0100

    Optimized imports

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import os
+from os.path import isfile
 
 from twitch_points_models import TwitchPointsModels
 from twitch_points_list import TwitchPointsList
 from gui.twitch_points_gui import TwitchPointsGUI
 
-
 if __name__ == "__main__":
     print("Twitch Points Tracker")
 
     twitch_points = TwitchPointsList()
-    if os.path.isfile("twitch_points.csv"):
+    if isfile("twitch_points.csv"):
         twitch_points.load_from_file("twitch_points.csv")
     else:
         twitch_points = TwitchPointsList()
 
     twitch_points_models = TwitchPointsModels()
-    if os.path.isfile("models.pkl"):
+    if isfile("models.pkl"):
         twitch_points_models.load_models()
     else:
         twitch_points_models = TwitchPointsModels()
